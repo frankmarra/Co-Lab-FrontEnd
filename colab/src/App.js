faec801bd67ed8c7e8bc76d6e5936cf39ac44d0d
Frank Marra <frankrmarra@gmail.com>
2022-05-07 21:22:33 -0400

    started styling login page

--- colab/src/App.js
+++ colab/src/App.js
@@ -3,19 +3,20 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
 import Home from './pages/Home'
+import UserProfile from './pages/UserProfile'
 
 const UserContext = createContext()
 
 function App() {
   const [user, setUser] = useState({})
 
+  // <Nav />
   return (
     <UserContext.Provider value={user}>
       <div className="App">
-        <Nav />
-
         <Routes>
           <Route path="/" element={<Home setUser={setUser} />} />
+          <Route path="/user/:id" element={<UserProfile />} />
         </Routes>
       </div>
     </UserContext.Provider>
--- colab/src/pages/Home.jsx
+++ colab/src/pages/Home.jsx
@@ -1,13 +1,34 @@
 import LoginSignup from '../components/LoginSignup'
 import Logout from '../components/Logout'
-import UserProfile from './UserProfile'
 
 const Home = ({ setUser }) => {
   return (
     <div className="home-wrapper">
-      <LoginSignup setUser={setUser} />
-      <Logout />
-      <UserProfile />
+      <div className="home-left">
+        <div className="home-image">
+          <img src="https://images.pexels.com/photos/4634553/pexels-photo-4634553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+          <div className="about-colab">words</div>
+        </div>
+      </div>
+      <div className="home-center">
+        <div className="home-title">Co-Lab</div>
+        <div className="loginout-buttons">
+          <LoginSignup setUser={setUser} />
+          <Logout />
+        </div>
+      </div>
+      <div className="home-right">
+        <div className="home-playlist">
+          <iframe
+            src="https://open.spotify.com/embed/playlist/3dcQJuWszkPX3H28QugNRr?utm_source=generator&theme=0"
+            width="100%"
+            height="380"
+            frameBorder="0"
+            allowFullScreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          ></iframe>
+        </div>
+      </div>
     </div>
   )
 }
